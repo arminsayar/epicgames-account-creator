d5a301544f4fa597d0d4decd7c4837f7ef6c45c0
Armin Sayar <saya.armin4@gmail.com>
2023-01-09 18:08:01 +0330

    code readibilty

--- index.js
+++ index.js
@@ -1,27 +1,43 @@
 const { Builder, By, Key, util } = require("selenium-webdriver");
 
+function idFinder(id) {
+    driver.findElement(By.id(id));
+};
+
+function xpathFinder(tag, text) {
+    driver.findElement(By.xpath(`//${tag}[text()='${text}']`));
+};
+
+function classFinder(className) {
+    driver.findElement(By.className(className));
+};
+
+function nameFinder(name) {
+    driver.findElement(By.name(name));
+};
+
 async function index() {
     let driver = await new Builder().forBrowser("chrome").build();
     await driver.get("https://www.epicgames.com/id/register/date-of-birth");
     await setTimeout(async () => {
-        await driver.findElement(By.id("year")).sendKeys("1999");
-        await driver.findElement(By.id("month")).click();
-        await driver.findElement(By.xpath("//li[text()='Jun']")).click();
-        await driver.findElement(By.id("day")).click();
-        await driver.findElement(By.xpath("//li[text()='8']")).click();
-        await driver.findElement(By.id("continue")).click();
+        await idFinder("year").sendKeys("1999");
+        await idFinder("month").click();
+        await xpathFinder("//li[text()='Jun']").click();
+        await idFinder("day").click();
+        await xpathFinder("//li[text()='8']").click();
+        await idFinder("continue").click();
     }, 1000)
     await setTimeout(async () => {
-        await driver.findElement(By.className("MuiButtonBase-root MuiIconButton-root MuiAutocomplete-popupIndicator")).click();
-        await driver.findElement(By.xpath("//li[text()='United States']")).click();
-        await driver.findElement(By.name("name")).sendKeys("Armin");
-        await driver.findElement(By.name("lastname")).sendKeys("Real");
-        await driver.findElement(By.name("displayname")).sendKeys("ArminIsReal");
-        await driver.findElement(By.name("email")).sendKeys("armin@real.com");
-        await driver.findElement(By.name("password")).sendKeys("Armin123456");
-        await driver.findElement(By.name("optIn")).click();
-        await driver.findElement(By.name("tos")).click();
-        await driver.findElement(By.id("btn-submit")).click();
+        await classFinder("MuiButtonBase-root MuiIconButton-root MuiAutocomplete-popupIndicator").click();
+        await xpathFinder("//li[text()='United States']").click();
+        await nameFinder("name").sendKeys("Armin");
+        await nameFinder("lastname").sendKeys("Real");
+        await nameFinder("displayname").sendKeys("ArminIsReal");
+        await nameFinder("email").sendKeys("armin@real.com");
+        await nameFinder("password").sendKeys("Armin123456");
+        await nameFinder("optIn").click();
+        await nameFinder("tos").click();
+        await idFinder("btn-submit").click();
     }, 1000)
 
 
